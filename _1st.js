3471931a7e06902f8a2695eeaf6bc67d8d62fd64
sohaib <71635873+sobi983@users.noreply.github.com>
2021-03-01 15:12:38 +0500

    Add files via upload

---
+++ _1st.js
@@ -0,0 +1,101 @@
+// 1st ways to print javascript
+// console.log("hello world");
+// alert("This is a alert");
+
+//2nd JS cocnsole API
+// console.log("Helo");
+// console.warn("This is a waning");
+// console.error("This is a error");
+
+//3rd JS variables
+// var num1=10;
+// var num2=20;
+// console.log(num1+num2);
+
+//4th Data variables
+// var s=("This is a string");
+// var t=true;
+// var marks={
+// sobi:11,
+// hassam:12,
+// srosh:13,
+// };
+// var n=null;
+
+//5th Data types
+// There are 2 types of data type
+// 1.Primitive data types(undifined,null,number,string,boolean,symbol)
+// var arr=[1,2,"sobi",4,5];
+// 2.Refrence data types(arrays and objects)
+
+//6th Operators(+,-,*,%,/)
+// var a=100;
+// var b=80;
+//Arthimatic operators
+// console.log("This is add",a+b);
+// console.log("This is substratc",a+b);
+// console.log("This is multiply",a+b);
+// console.log("This is dicvide",a+b);
+
+//Assignment operators
+// var c=b;
+// c +=2;
+// c -=2;
+// c *=3;
+// c /=2;
+
+//Comparison operators
+// var q=10;
+// var w=10;
+// console.log(q==w);
+// console.log(q!=w);
+// console.log(q>w);
+// console.log(q>=w);
+
+//Logical operator
+// console.log(true && true);
+// console.log(true || true);
+// console.log(!true);
+
+//6th functions
+// function avg(a,b){
+//   c=(a+b)/2;
+//   return c;
+// }
+// var c1=avg(10,2);
+// console.log(c1);
+
+//Conditional statements If/else
+// var age=10;
+// if(age>9)
+// console.log("age is greater than 10");
+// else
+// {console.log("Age is less than 10")};
+
+//7th Loop
+// var arr=[1,2,3,4,5,6,7];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+//8th Array methods
+// arr.length();
+// arr.pop();
+// arr.push();
+
+//9 thString methods
+// var my="My name is sobi";
+// console.log(my.length);
+// console.log(my.indexOf("sobi"));
+// console.log(my.slice(0,7));
+// console.log(my.replace("sobi","Usama"));
+
+//10th DOM manipulation
+// document.getElementById("demo").innerHTML = "Hello World"; 
+//  (changes the written content into Hello world)(getElementById is a method and .innerHTML is a property)
+// let elemClass = document.getElementsByClassName("container");
+// console.log(elemClass);
+// elemClass[0].style.background="black";
+// elemClass[0].classList.add("check");
+// elemClass[0].classList.add("check2");
+// elemClass[1].classList.add("check2");
